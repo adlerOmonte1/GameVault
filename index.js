5ea5a49084954e015698d3e32ea099e298088193
adlerOmonte1 <klin.forever24@gmail.com>
2026-03-26 22:24:30 -0500

    Initialize GameVault project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler'; // to indicate that there will be navigation
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
