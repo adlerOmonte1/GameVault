210298755ea2096b19f01a9dce1710e2c353309e
adlerOmonte1 <klin.forever24@gmail.com>
2026-03-28 02:30:02 -0500

    Modification of styles of the GameListStyles.js and GamingNewStyles.js files and link of the Ionicons source in the Info.plist of the IOS folder

--- src/Styles/GamingNewsStyles.js
+++ src/Styles/GamingNewsStyles.js
@@ -4,31 +4,50 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1, 
+    backgroundColor: "#d1d1d1"
   },
   card: {
-    borderWidth: 1,
-    borderColor: 'black', 
-    margin: 10,
-    padding: 10,
+    padding: 20,
+    marginHorizontal:30,
+    marginBottom:10,
+    marginVertical:150,
+    backgroundColor:"#ffff",
+    borderRadius: 25,
+
   },
   indicatorText: {
-    color: 'black',
+    textAlign:'center',
+    fontSize: 14,
+    color: "#555555",
+    marginTop:5,
+    marginBottom:10,
+    fontWeight:'800'
   },
   newsTitle: {
-    color: 'black',
-    fontWeight: 'bold', 
+    fontSize:18,
+    fontWeight:'bold',
+    marginBottom:10,
+    textAlign: 'center'
   },
   newsSummary: {
+    fontSize:15,
     color: 'black',
+    lineHeight:22,
+    textAlign:'center'
   },
   nextButton: {
-    borderWidth: 1,
-    borderColor: 'black',
-    margin: 10,
-    padding: 10,
+    backgroundColor: '#4CAF50',
+    marginHorizontal: 60,
+    marginTop:10,
+    padding:15,
+    borderRadius: 20,
+    alignItems:'center'
+
   },
   nextButtonText: {
-    color: 'black',
+    color: '#ffff',
+    fontWeight:'bold',
+    fontSize: 16,
   }
 });
 
